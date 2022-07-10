09a8a48106aa12a4df51bc4fd46ac83dcaf84fe9
SoloPimpa <sdarom.sdarom@gmail.com>
2022-07-10 23:14:46 +0300

    footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        autoplay:true,
+        loop: true,
+        margin: 0,
+        nav: true,
+        autoplay: true,
         autoplayTimeout: 2000,
-        navText: [ '', ' ' ],
-
-        responsive:{
-            0:{
-                items:1
+        navText: ['', ' '],
+        responsive: {
+            0: {
+                items: 1
             },
-
-            1000:{
-                items:4
+            360: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            1000: {
+                items: 4
             }
         }
     });
 });
 const hamburgerButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
-function toggleMenu(){
+
+function toggleMenu() {
     console.log('toggle');
     menu.classList.toggle('hidden');
     document.body.classList.toggle('no-scroll');
 }
+
 hamburgerButton.addEventListener('click', toggleMenu);
